dda63a8cb6c9c2de3d3ee7b1baeace349f4a0d18
Aneesh <alodhavia00@gmail.com>
2017-10-05 10:59:09 -0700

    adding javafx...

--- src/LineRider.java
+++ src/LineRider.java
@@ -1,17 +1,68 @@
-public class LineRider {
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.stage.Stage;
+
+public class LineRider extends Application {
+    Path path;
+
     public static void main(String args[]) {
         Ticker ticker = new Ticker(20); // 20 ticks per second
 
         ticker.addTickListener(new TickListener() {
-
-            @Override
-            public void onTick(float deltaTime) {
-                System.out.println(String.format("Ticked with deltaTime %f", deltaTime));
-            }
-        });
-        
+                                   @Override
+                                   public void onTick(float deltaTime) {
+                                       System.out.println(String.format("Ticked with deltaTime %f", deltaTime));
+                                   }
+                               }
+        );
+        launch(args);
         while (true) {
             ticker.update();
         }
+
     }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Group root = new Group();
+        Scene scene = new Scene(root, 300, 250);
+
+        path = new Path();
+        path.setStrokeWidth(1);
+        path.setStroke(Color.BLACK);
+
+        scene.setOnMouseClicked(mouseHandler);
+        scene.setOnMouseDragged(mouseHandler);
+        scene.setOnMouseEntered(mouseHandler);
+        scene.setOnMouseExited(mouseHandler);
+        scene.setOnMouseMoved(mouseHandler);
+        scene.setOnMousePressed(mouseHandler);
+        scene.setOnMouseReleased(mouseHandler);
+
+        root.getChildren().add(path);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    EventHandler<MouseEvent> mouseHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent mouseEvent) {
+            if (mouseEvent.getEventType() == MouseEvent.MOUSE_PRESSED) {
+                path.getElements().clear();
+                path.getElements()
+                        .add(new MoveTo(mouseEvent.getX(), mouseEvent.getY()));
+            } else if (mouseEvent.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+                path.getElements()
+                        .add(new LineTo(mouseEvent.getX(), mouseEvent.getY()));
+            }
+
+        }
+    };
 }
