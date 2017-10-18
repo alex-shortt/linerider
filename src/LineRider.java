b9d6d2296feb6ed975e097900393eb0775f847d4
Alex Shortt <Alex Shortt>
2017-10-17 21:07:48 -0700

    collisions with lines working

--- src/LineRider.java
+++ src/LineRider.java
@@ -4,82 +4,150 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Bounds;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-public class LineRider extends Application{
-    public static void main(String[] args){
-        launch(args);
-    }
-    //----------------------------------------------------//
-    public static Circle circle;
-    public static Rectangle rectangle;
-    public static Pane canvas;
-    private long counter = 0;
-    double X = 0;
-    double Y = 0;
+import java.util.ArrayList;
 
-    final static int WIDTH = 800;
-    final static int HEIGHT = 800;
+public class LineRider extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        canvas = new Pane();
-        Scene scene = new Scene(canvas, WIDTH,WIDTH);
+    private final int LINE_LENGTH = 16;
+    private static Circle ball = new Circle(15, Color.BLUE);
+    private final int HEIGHT = 800;
+    private final int WIDTH = 800;
+    private static Group root;
 
-        primaryStage.setTitle("Bouncing balls at angels");
+
+    public void start(Stage primaryStage) {
+        root = new Group();
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        final GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+
+        ball.relocate(100, 100);
+        root.getChildren().addAll(ball);
+        root.getChildren().add(canvas);
+
+
         primaryStage.setScene(scene);
+        primaryStage.setTitle("LineRider Prototype v0.1");
         primaryStage.show();
 
-        circle = new Circle(15, Color.BLUE);
-        circle.relocate(200,200);
+        ArrayList<Line> lines = new ArrayList<>();
+        double[] startCoords = new double[2];
 
-        //rectangle = new Rectangle(750, 500);
-        //rectangle.relocate(100,100);
+        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
+            startCoords[0] = event.getX();
+            startCoords[1] = event.getY();
+            //sets the starting position of the line, just to get it off the ground
+        });
 
-        //canvas.getChildren().addAll(rectangle);
-        canvas.getChildren().addAll(circle);
+        canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, event -> {
+            if (Math.hypot(startCoords[0] - event.getX(), startCoords[1] - event.getY()) > LINE_LENGTH) {
+                graphicsContext.strokeLine(startCoords[0], startCoords[1], event.getX(), event.getY());
+                lines.add(new Line(startCoords[0], startCoords[1], event.getX(), event.getY()));
+
+                startCoords[0] = event.getX();
+                startCoords[1] = event.getY();
+
+                //just prints out how big the arraylist is so i know this works
+                System.out.println(lines.size());
+            }
+        });
+
+        canvas.addEventHandler(MouseEvent.MOUSE_RELEASED, event -> {
+            //empty, but you can do shit here if you want to have something upon mouse release
+        });
+
 
         final Timeline loop = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>() {
-
-            double deltaX = .5;
-            double deltaY = 0;
-            double gravity = .15;
-            double Yfriction = .07;
-            final double RADIUS = circle.getRadius();
+            double velX = .5;
+            double velY = 0;
+            double accelY = .15;
+            double frictionY = .965;
+            double frictionX = .999;
+            final double RADIUS = ball.getRadius();
 
             //@Override
             public void handle(final ActionEvent t) {
-                circle.setLayoutX(circle.getLayoutX() + deltaX);
-                circle.setLayoutY(circle.getLayoutY() + deltaY);
+                ball.setLayoutX(ball.getLayoutX() + velX);
+                ball.setLayoutY(ball.getLayoutY() + velY);
 
-                deltaY += gravity;
+                velY += accelY;
 
                 final Bounds bounds = canvas.getBoundsInLocal();
-                final boolean atRightBorder = circle.getLayoutX() >= (bounds.getMaxX() - RADIUS);
-                final boolean atLeftBorder = circle.getLayoutX() <= RADIUS;
-                final boolean atBottomBorder = circle.getLayoutY() >= (bounds.getMaxY() - RADIUS);
-                final boolean atTopBorder = circle.getLayoutY() <= RADIUS;
+                final boolean atRightBorder = ball.getLayoutX() >= (bounds.getMaxX() - RADIUS);
+                final boolean atLeftBorder = ball.getLayoutX() <= RADIUS;
+                final boolean atBottomBorder = ball.getLayoutY() >= (bounds.getMaxY() - RADIUS);
+                final boolean atTopBorder = ball.getLayoutY() <= RADIUS;
 
-                if (atRightBorder || atLeftBorder) {
-                    deltaX *= -1;
+                if (atRightBorder) {
+                    ball.setLayoutX(bounds.getMaxX() - (RADIUS * 1.1));
+                    velX *= frictionX;
+                    velX *= -1;
                 }
-                if (atBottomBorder || atTopBorder) {
-                    deltaY *= -1;
-                    if(deltaY<0)deltaY+=Yfriction;
-                    if(deltaY>0)deltaY-=Yfriction;
-                    if(atBottomBorder){
-                        circle.setLayoutY(bounds.getMaxY() - RADIUS);
-                    }
+                if (atLeftBorder) {
+                    ball.setLayoutX(RADIUS * 1.1);
+                    velX *= frictionX;
+                    velX *= -1;
                 }
+                if (atBottomBorder) {
+                    velY *= -1;
+                    velY *= frictionY;
+                    ball.setLayoutY(bounds.getMaxY() - (RADIUS * 1.1));
+                }
+                if (atTopBorder) {
+                    velY *= -1;
+                    velY *= frictionY;
+                    ball.setLayoutY(RADIUS * 1.1);
+                }
+
+                checkCollisions(ball, lines);
             }
         }));
+
         loop.setCycleCount(Timeline.INDEFINITE);
         loop.play();
+    }
+
+    public void checkCollisions(Circle ball, ArrayList<Line> lines) {
+        ball.setFill(Color.BLUE);
+        for (Line line : lines) {
+            Shape intersect = Shape.intersect(line, ball);
+            if (intersect.getBoundsInLocal().getWidth() != -1 || intersect.getBoundsInLocal().getHeight() != -1) {
+                Line newLine = new Line(line.getStartX(), line.getStartY(), line.getEndX(), line.getEndY());
+                newLine.setStroke(Color.GREEN);
+                newLine.setStrokeWidth(5);
+                root.getChildren().add(newLine);
+
+                double lineMidX = (line.getStartX() + line.getEndX()) / 2;
+                double lineMidY = (line.getStartY() + line.getEndY()) / 2;
+                double ballMidX = ball.getLayoutX();
+                double ballMidY = ball.getLayoutY();
+
+                ball.setFill(Color.RED);
+                /*
+                while (ball.getBoundsInParent().intersects(line.getBoundsInParent())) {
+                    System.out.println("Distance: " + Math.sqrt(Math.pow(ballMidX - lineMidX, 2) + Math.pow(ballMidY - lineMidY, 2)));
+                    ball.setLayoutX(ball.getLayoutX() + ((ballMidX - lineMidX) * 0.001));
+                    ball.setLayoutY(ball.getLayoutY() + ((ballMidY - lineMidY) * 0.001));
+                }
+                */
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
